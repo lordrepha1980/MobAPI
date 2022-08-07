6d6be205a61ca4fb0c620ce880d91898efdd3397
lordrepha1980 <freelancer@christoph-duengel.de>
2022-08-07 11:39:29 +0200

    serverport

--- server/database/MongoDB/Connection.js
+++ server/database/MongoDB/Connection.js
@@ -10,7 +10,7 @@ module.exports = class Connection {
 
         async init() {
             const { MongoClient }   = require('mongodb');
-            const url               = `mongodb://${config.database.host}:${config.database.port}/${config.database.name}`;
+            let url               = `mongodb://${config.database.host}:${config.database.port}/${config.database.name}`;
 
             if ( config.database.credentials )
                 url               = `mongodb://${config.database.credentials.user}:${config.database.credentials.password}@${config.database.host}:${config.database.port}/${config.database.name}`;
