40c412678bc6c9977e664e86912793bd24db0a0d
christoph.duengel <freelancer@christoph-duengel.de>
2022-09-13 15:03:54 +0200

    Filupload done

--- routes/custom.js
+++ routes/custom.js
@@ -28,4 +28,21 @@ router
         }
     })
 
+router
+    .get("/:class/:action", async (ctx, next) => {
+        
+        const Custom        = require(`${_dirname}/server/database/customApi/${ctx.params.class}.js`);
+        const custom        = new Custom();
+        let result          = {data: null};
+
+        if ( custom[ctx.params.action] )
+            result.data = await custom[ctx.params.action]( { query: ctx.request.query, ctx, io } )
+        else {
+            console.error( 'No action found. Called action: ', ctx.params.action )
+
+            ctx.status  = result.error ? 400 : 200;
+            ctx.body    = result;
+        }
+    })
+
 module.exports = router;
