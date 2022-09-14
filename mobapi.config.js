519b73596e273d33e3a65c5f9aa5f52b142af9ae
christoph.duengel <freelancer@christoph-duengel.de>
2022-09-14 12:00:08 +0200

    Custom Route Get/Post

--- mobapi.config.js
+++ mobapi.config.js
@@ -6,7 +6,6 @@ module.exports = {
         watch  : true,
         ignore_watch: [
             "./server/database/MongoDB/dataApi/*",
-            "./server/database/sqlite/dataApi/*",
             "./server/database/customApi/*",
             "node_modules"
         ]
