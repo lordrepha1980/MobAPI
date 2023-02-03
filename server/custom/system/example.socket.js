21e1fb0643a4add6a481a06b35ef884e839f89f6
christoph.duengel <freelancer@christoph-duengel.de>
2023-02-03 13:38:26 +0100

    Bugfix socket

--- server/custom/system/example.socket.js
+++ server/custom/system/example.socket.js
@@ -1,27 +1,31 @@
-const debug             = require('debug')('app:server:custom/system:socket');
-const IO                = require('koa-socket-2');
-const io                = new IO({
-    ioOptions: {
-      cors: {},
-    },
-});
+const { Server }            = require('socket.io');
+const dotenv                = require('dotenv')
+dotenv.config({ path: './.env' })
 
+let options = null
+if ( process.env.DEV )
+    options = {
+        cors: {
+            origin: `${ process.env.DEV ? 'http' : 'https' }://${process.env.SERVER_HOST}:${process.env.CLIENT_PORT}`,
+            credentials: true
+        }
+    }
+
+const io                    = new Server(options);
 
 module.exports  = ( app ) => {
     io.attach(app);
-    debug('socket io attached');
+    console.log('socket io attached');
    
     io.on("connection", (socket) => {
         sock = socket;
-        debug('socket connected');
+        io.socket = socket;
+        console.log('socket connected');
+    
     });
 
     io.on("disconnect", (socket) => {
-        debug('socket disconnected');
-    });
-
-    io.on("message", (ctx, data) => {
-        debug(data);
+        console.log('socket disconnected');
     });
 
     return io
